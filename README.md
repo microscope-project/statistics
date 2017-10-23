42a796a8b58dd60ec87a2f4c7924a8eca5507624
hyj1991 <hugh.hyj@alibaba-inc.com>
2017-10-23 16:19:51 +0800

    简单初始化项目

---
+++ README.md
@@ -0,0 +1,13 @@
+# 下载统计
+
+快速开始
+
+```
+node dispatch.js
+```
+
+访问
+
+```
+http://localhost:3000
+```
---
+++ dispatch.js
@@ -0,0 +1,49 @@
+'use strict';
+const path = require('path');
+const fastify = require('fastify')();
+const serveStatic = require('serve-static');
+const urllib = require('urllib');
+
+fastify.use(['/css', '/js'], serveStatic(path.join(__dirname, './assets')));
+
+fastify.register(require('point-of-view'), {
+  engine: {
+    ejs: require('ejs')
+  }
+});
+
+fastify.get('/', async function (req, rep) {
+  const yesterday = new Date(Date.now() - 24 * 60 * 60 * 1000);
+  const format = `${yesterday.getFullYear()}-${yesterday.getMonth() + 1}-${yesterday.getDate()}`;
+  // 2017.3.10 发布
+  const startMonth = new Date('2017-03-10');
+  const lastMonth = yesterday.getMonth() && new Date(`${yesterday.getFullYear()}-${yesterday.getMonth()}-${yesterday.getDate()}`)
+    || new Date(`${yesterday.getFullYear() - 1}-12-${yesterday.getDate()}`);
+  const urlAll = `http://api.npmjs.org/downloads/point/2017-03-10:${format}/easy-monitor`;
+  const urlDay = `http://api.npmjs.org/downloads/range/2017-03-10:${format}/easy-monitor`;
+  const results = await Promise.all([urllib.request(urlAll), urllib.request(urlDay)]);
+  const all = JSON.parse(String(results[0].data));
+  const day = JSON.parse(String(results[1].data));
+  rep.view(path.join('/view/statistics_day.html'), {
+    start: ((lastMonth - startMonth) / (yesterday - startMonth)) * 100,
+    ezm: {
+      day: {
+        date: JSON.stringify(day.downloads.map(r => r.day)),
+        data: JSON.stringify(day.downloads.map(r => r.downloads))
+      },
+      all: {
+        date: JSON.stringify(day.downloads.map(r => r.day)),
+        data: JSON.stringify(day.downloads.reduce((p, r) => {
+          p.c += Number(r.downloads);
+          p.a.push(p.c);
+          return p;
+        }, { c: 0, a: [] }).a)
+      }
+    }
+  });
+});
+
+fastify.listen(3000, function (err) {
+  if (err) throw err
+  console.log(`server listening on ${fastify.server.address().port}`)
+})
